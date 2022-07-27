108987275d281239bab9b0d5cc30b32aba54fc87
andylin-hao <linhao-16@outlook.com>
2022-07-28 04:20:24 +0800

    Add autocompletion for package list.

--- src/main/java/com/android/androidperf/AppController.java
+++ src/main/java/com/android/androidperf/AppController.java
@@ -2,6 +2,7 @@ package com.android.androidperf;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Side;
@@ -9,7 +10,10 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
+import org.controlsfx.control.textfield.TextFields;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -143,6 +147,7 @@ public class AppController implements Initializable {
             selectedDevice.endPerf();
         String deviceID = deviceListBox.getSelectionModel().getSelectedItem();
         selectedDevice = deviceMap.get(deviceID);
+        new AutoCompleteComboBoxListener<>(packageListBox);
 
         // register perf services
         selectedDevice.registerService(FPSPerfService.class);
@@ -245,5 +250,79 @@ public class AppController implements Initializable {
 
     public void shutdown() {
         deviceMap.forEach((s, device) -> device.shutdown());
+    }
+
+    static class AutoCompleteComboBoxListener<T> implements EventHandler<KeyEvent> {
+        private final ComboBox<T> comboBox;
+        private final ObservableList<T> data;
+        private boolean moveCaretToPos = false;
+        private int caretPos;
+
+        public AutoCompleteComboBoxListener(final ComboBox<T> comboBox) {
+            this.comboBox = comboBox;
+            data = comboBox.getItems();
+
+            this.comboBox.setEditable(true);
+            this.comboBox.setOnKeyPressed(t -> comboBox.hide());
+            this.comboBox.setOnKeyReleased(AutoCompleteComboBoxListener.this);
+        }
+
+        @Override
+        public void handle(KeyEvent event) {
+            if(event.getCode() == KeyCode.UP) {
+                caretPos = -1;
+                moveCaret(comboBox.getEditor().getText().length());
+                return;
+            } else if(event.getCode() == KeyCode.DOWN) {
+                if(!comboBox.isShowing()) {
+                    comboBox.show();
+                }
+                caretPos = -1;
+                moveCaret(comboBox.getEditor().getText().length());
+                return;
+            } else if(event.getCode() == KeyCode.BACK_SPACE) {
+                moveCaretToPos = true;
+                caretPos = comboBox.getEditor().getCaretPosition();
+            } else if(event.getCode() == KeyCode.DELETE) {
+                moveCaretToPos = true;
+                caretPos = comboBox.getEditor().getCaretPosition();
+            }
+
+            if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT
+                    || event.isControlDown() || event.getCode() == KeyCode.HOME
+                    || event.getCode() == KeyCode.END || event.getCode() == KeyCode.TAB) {
+                return;
+            }
+
+            ObservableList<T> list = FXCollections.observableArrayList();
+            for (T datum : data) {
+                if (datum.toString().toLowerCase().contains(
+                        AutoCompleteComboBoxListener.this.comboBox
+                                .getEditor().getText().toLowerCase())) {
+                    list.add(datum);
+                }
+            }
+            String t = comboBox.getEditor().getText();
+
+            comboBox.setItems(list);
+            comboBox.getEditor().setText(t);
+            if(!moveCaretToPos) {
+                caretPos = -1;
+            }
+            moveCaret(t.length());
+            if(!list.isEmpty()) {
+                comboBox.show();
+            }
+        }
+
+        private void moveCaret(int textLength) {
+            if(caretPos == -1) {
+                comboBox.getEditor().positionCaret(textLength);
+            } else {
+                comboBox.getEditor().positionCaret(caretPos);
+            }
+            moveCaretToPos = false;
+        }
+
     }
 }
